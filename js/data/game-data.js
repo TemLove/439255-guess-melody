ecc7dd9f79b69bcb256f8de29db03b4fe3a56be3
TemLove <temnova.hi@gmail.com>
2018-06-09 00:27:30 +0700

    Добавляет функцию отображения времени, вносит правки в подсчете времени

--- js/data/game-data.js
+++ js/data/game-data.js
@@ -1,6 +1,6 @@
 export const gameOptions = {
-  timeAll: 300000,
-  timeLimitForQuickAnswer: 30000,
+  timeAll: 300,
+  timeLimitForQuickAnswer: 30,
   levelsCount: 10,
   attemptsCount: 3,
   scoreForRightAnswer: 1,
@@ -14,42 +14,45 @@ export const countResultScore = (userAnswers, attemptsLeft) => {
   }
 
   return userAnswers.reduce((acc, it) => {
-    switch (true) {
-      case it.isAnswerRight && it.spendedTime < gameOptions.timeLimitForQuickAnswer :
-        acc += gameOptions.scoreForQuickRightAnswer;
-        break;
-      case it.isAnswerRight :
-        acc += gameOptions.scoreForRightAnswer;
-        break;
-      case !it.isAnswerRight :
-        acc += gameOptions.scoreForWrongAnswer;
-        break;
+    if (it.isAnswerRight && it.spendedTime < gameOptions.timeLimitForQuickAnswer) {
+      acc += gameOptions.scoreForQuickRightAnswer;
     }
+
+    if (it.isAnswerRight && it.spendedTime >= gameOptions.timeLimitForQuickAnswer) {
+      acc += gameOptions.scoreForRightAnswer;
+    }
+
+    if (!it.isAnswerRight) {
+      acc += gameOptions.scoreForWrongAnswer;
+    }
+
     return acc;
   }, 0);
 };
 
 export const getResultMessage = (gameResults, userResult) => {
-  switch (0) {
-    case userResult.timeLeft :
-      return `Время вышло! Вы не успели отгадать все мелодии`;
-    case userResult.attemptsLeft :
-      return `У вас закончились все попытки. Ничего, повезет в следующи раз!`;
-    default :
-      let results = gameResults.slice();
-      results.push(userResult.score);
-      results.sort((a, b) => a - b);
-      const userPosition = results.length - results.indexOf(userResult.score);
-      const playersCount = results.length;
-      const successPercent = ((playersCount - userPosition) / playersCount).toFixed(2) * 100;
-      return `Вы заняли ${userPosition} место из ${playersCount}. Это лучше, чем у ${successPercent}% игроков`;
+  if (userResult.timeLeft === 0) {
+    return `Время вышло! Вы не успели отгадать все мелодии`;
   }
+
+  if (userResult.attemptsLeft === 0) {
+    return `У вас закончились все попытки. Ничего, повезет в следующи раз!`;
+  }
+
+  let results = gameResults.slice();
+  results.push(userResult.score);
+  results.sort((a, b) => a - b);
+
+  const userPosition = results.length - results.indexOf(userResult.score);
+  const playersCount = results.length;
+  const successPercent = ((playersCount - userPosition) / playersCount).toFixed(2) * 100;
+
+  return `Вы заняли ${userPosition} место из ${playersCount}. Это лучше, чем у ${successPercent}% игроков`;
 };
 
-export const getTimer = (time = gameOptions.timeAll, tickTime = 1000) => {
+export const getTimer = (time = gameOptions.timeAll) => {
   return {
     _time: time >= 0 ? time : -time,
-    _tickTime: tickTime >= 0 ? tickTime : -tickTime,
     get time() {
       return this._time > 0 ? this._time : -1;
     },
@@ -60,19 +63,27 @@ export const getTimer = (time = gameOptions.timeAll, tickTime = 1000) => {
         throw new Error(`Time should be possitive`);
       }
     },
-    get tickTime() {
-      return this._tickTime;
-    },
-    set tickTime(newTime) {
-      if (newTime >= 0) {
-        this._tickTime = newTime;
-      } else {
-        throw new Error(`Time should be possitive`);
-      }
-    },
     tick() {
-      this._time -= this._tickTime;
+      this._time -= 1;
       return this._time > 0 ? this._time : -1;
     }
   };
+};
+
+const getWordEnding = (num) => {
+  if ((num > 10 && num <= 20) || num % 10 > 4 || num % 10 === 0) {
+    return ``;
+  }
+  if (num % 10 === 1) {
+    return `у`;
+  }
+  return `ы`;
+};
+
+export const getTimeString = (time) => {
+  const second = time % 60;
+  const minute = Math.trunc(time / 60);
+
+  return `за ${minute} минут${getWordEnding(minute)} и \
+${`${second}`.length < 2 ? `0${second}` : second} секунд${getWordEnding(second)}`;
 };
--- js/data/game-data.test.js
+++ js/data/game-data.test.js
@@ -1,124 +1,124 @@
 import {assert} from 'chai';
-import {countResultScore, getResultMessage, getTimer} from './game-data';
+import {countResultScore, getResultMessage, getTimer, getTimeString} from './game-data';
 
 describe(`Result count function`, () => {
   it(`Should return -1 when answers count less than 10`, () => {
-    assert.strictEqual(-1, countResultScore([{isAnswerRight: true, spendedTime: 30000},
-      {isAnswerRight: true, spendedTime: 30000},
-      {isAnswerRight: true, spendedTime: 30000},
-      {isAnswerRight: true, spendedTime: 30000},
-      {isAnswerRight: true, spendedTime: 30000},
-      {isAnswerRight: true, spendedTime: 30000},
-      {isAnswerRight: true, spendedTime: 30000},
-      {isAnswerRight: true, spendedTime: 30000},
-      {isAnswerRight: true, spendedTime: 60000}], 3));
-    assert.strictEqual(-1, countResultScore([{isAnswerRight: true, spendedTime: 30000},
-      {isAnswerRight: true, spendedTime: 30000},
-      {isAnswerRight: true, spendedTime: 30000},
-      {isAnswerRight: true, spendedTime: 30000},
-      {isAnswerRight: true, spendedTime: 30000},
-      {isAnswerRight: true, spendedTime: 30000},
-      {isAnswerRight: true, spendedTime: 30000},
-      {isAnswerRight: false, spendedTime: 90000}], 2));
-    assert.strictEqual(-1, countResultScore([{isAnswerRight: true, spendedTime: 30000},
-      {isAnswerRight: true, spendedTime: 30000},
-      {isAnswerRight: true, spendedTime: 30000},
-      {isAnswerRight: true, spendedTime: 30000},
-      {isAnswerRight: true, spendedTime: 30000},
-      {isAnswerRight: false, spendedTime: 60000},
-      {isAnswerRight: false, spendedTime: 90000}], 1));
+    assert.strictEqual(-1, countResultScore([{isAnswerRight: true, spendedTime: 30},
+      {isAnswerRight: true, spendedTime: 30},
+      {isAnswerRight: true, spendedTime: 30},
+      {isAnswerRight: true, spendedTime: 30},
+      {isAnswerRight: true, spendedTime: 30},
+      {isAnswerRight: true, spendedTime: 30},
+      {isAnswerRight: true, spendedTime: 30},
+      {isAnswerRight: true, spendedTime: 30},
+      {isAnswerRight: true, spendedTime: 60}], 3));
+    assert.strictEqual(-1, countResultScore([{isAnswerRight: true, spendedTime: 30},
+      {isAnswerRight: true, spendedTime: 30},
+      {isAnswerRight: true, spendedTime: 30},
+      {isAnswerRight: true, spendedTime: 30},
+      {isAnswerRight: true, spendedTime: 30},
+      {isAnswerRight: true, spendedTime: 30},
+      {isAnswerRight: true, spendedTime: 30},
+      {isAnswerRight: false, spendedTime: 90}], 2));
+    assert.strictEqual(-1, countResultScore([{isAnswerRight: true, spendedTime: 30},
+      {isAnswerRight: true, spendedTime: 30},
+      {isAnswerRight: true, spendedTime: 30},
+      {isAnswerRight: true, spendedTime: 30},
+      {isAnswerRight: true, spendedTime: 30},
+      {isAnswerRight: false, spendedTime: 60},
+      {isAnswerRight: false, spendedTime: 90}], 1));
   });
 
   it(`Should return -1 when no more attempts left`, () => {
-    assert.strictEqual(-1, countResultScore([{isAnswerRight: true, spendedTime: 30000},
-      {isAnswerRight: true, spendedTime: 30000},
-      {isAnswerRight: true, spendedTime: 30000},
-      {isAnswerRight: true, spendedTime: 30000},
-      {isAnswerRight: true, spendedTime: 30000},
-      {isAnswerRight: true, spendedTime: 30000},
-      {isAnswerRight: true, spendedTime: 30000},
-      {isAnswerRight: false, spendedTime: 30000},
-      {isAnswerRight: false, spendedTime: 30000},
-      {isAnswerRight: false, spendedTime: 20000}], 0));
+    assert.strictEqual(-1, countResultScore([{isAnswerRight: true, spendedTime: 30},
+      {isAnswerRight: true, spendedTime: 30},
+      {isAnswerRight: true, spendedTime: 30},
+      {isAnswerRight: true, spendedTime: 30},
+      {isAnswerRight: true, spendedTime: 30},
+      {isAnswerRight: true, spendedTime: 30},
+      {isAnswerRight: true, spendedTime: 30},
+      {isAnswerRight: false, spendedTime: 30},
+      {isAnswerRight: false, spendedTime: 30},
+      {isAnswerRight: false, spendedTime: 20}], 0));
   });
 
-  it(`Should count 2 points for correct answer when spended time less than 30000 ms`, () => {
-    assert.strictEqual(20, countResultScore([{isAnswerRight: true, spendedTime: 20000},
-      {isAnswerRight: true, spendedTime: 20000},
-      {isAnswerRight: true, spendedTime: 20000},
-      {isAnswerRight: true, spendedTime: 20000},
-      {isAnswerRight: true, spendedTime: 20000},
-      {isAnswerRight: true, spendedTime: 20000},
-      {isAnswerRight: true, spendedTime: 20000},
-      {isAnswerRight: true, spendedTime: 20000},
-      {isAnswerRight: true, spendedTime: 20000},
-      {isAnswerRight: true, spendedTime: 20000}], 3));
+  it(`Should count 2 points for correct answer when spended time less than 30 second`, () => {
+    assert.strictEqual(20, countResultScore([{isAnswerRight: true, spendedTime: 20},
+      {isAnswerRight: true, spendedTime: 20},
+      {isAnswerRight: true, spendedTime: 20},
+      {isAnswerRight: true, spendedTime: 20},
+      {isAnswerRight: true, spendedTime: 20},
+      {isAnswerRight: true, spendedTime: 20},
+      {isAnswerRight: true, spendedTime: 20},
+      {isAnswerRight: true, spendedTime: 20},
+      {isAnswerRight: true, spendedTime: 20},
+      {isAnswerRight: true, spendedTime: 20}], 3));
   });
 
-  it(`Should count 1 points for correct answer when spended time more than 30000 ms`, () => {
-    assert.strictEqual(20, countResultScore([{isAnswerRight: true, spendedTime: 20000},
-      {isAnswerRight: true, spendedTime: 20000},
-      {isAnswerRight: true, spendedTime: 20000},
-      {isAnswerRight: true, spendedTime: 20000},
-      {isAnswerRight: true, spendedTime: 20000},
-      {isAnswerRight: true, spendedTime: 20000},
-      {isAnswerRight: true, spendedTime: 20000},
-      {isAnswerRight: true, spendedTime: 20000},
-      {isAnswerRight: true, spendedTime: 20000},
-      {isAnswerRight: true, spendedTime: 20000}], 3));
-    assert.strictEqual(19, countResultScore([{isAnswerRight: true, spendedTime: 20000},
-      {isAnswerRight: true, spendedTime: 20000},
-      {isAnswerRight: true, spendedTime: 20000},
-      {isAnswerRight: true, spendedTime: 20000},
-      {isAnswerRight: true, spendedTime: 20000},
-      {isAnswerRight: true, spendedTime: 20000},
-      {isAnswerRight: true, spendedTime: 20000},
-      {isAnswerRight: true, spendedTime: 20000},
-      {isAnswerRight: true, spendedTime: 20000},
-      {isAnswerRight: true, spendedTime: 40000}], 3));
-    assert.strictEqual(18, countResultScore([{isAnswerRight: true, spendedTime: 20000},
-      {isAnswerRight: true, spendedTime: 20000},
-      {isAnswerRight: true, spendedTime: 20000},
-      {isAnswerRight: true, spendedTime: 20000},
-      {isAnswerRight: true, spendedTime: 40000},
-      {isAnswerRight: true, spendedTime: 20000},
-      {isAnswerRight: true, spendedTime: 20000},
-      {isAnswerRight: true, spendedTime: 20000},
-      {isAnswerRight: true, spendedTime: 20000},
-      {isAnswerRight: true, spendedTime: 40000}], 3));
+  it(`Should count 1 points for correct answer when spended time more than 30 second`, () => {
+    assert.strictEqual(20, countResultScore([{isAnswerRight: true, spendedTime: 20},
+      {isAnswerRight: true, spendedTime: 20},
+      {isAnswerRight: true, spendedTime: 20},
+      {isAnswerRight: true, spendedTime: 20},
+      {isAnswerRight: true, spendedTime: 20},
+      {isAnswerRight: true, spendedTime: 20},
+      {isAnswerRight: true, spendedTime: 20},
+      {isAnswerRight: true, spendedTime: 20},
+      {isAnswerRight: true, spendedTime: 20},
+      {isAnswerRight: true, spendedTime: 20}], 3));
+    assert.strictEqual(19, countResultScore([{isAnswerRight: true, spendedTime: 20},
+      {isAnswerRight: true, spendedTime: 20},
+      {isAnswerRight: true, spendedTime: 20},
+      {isAnswerRight: true, spendedTime: 20},
+      {isAnswerRight: true, spendedTime: 20},
+      {isAnswerRight: true, spendedTime: 20},
+      {isAnswerRight: true, spendedTime: 20},
+      {isAnswerRight: true, spendedTime: 20},
+      {isAnswerRight: true, spendedTime: 20},
+      {isAnswerRight: true, spendedTime: 40}], 3));
+    assert.strictEqual(18, countResultScore([{isAnswerRight: true, spendedTime: 20},
+      {isAnswerRight: true, spendedTime: 20},
+      {isAnswerRight: true, spendedTime: 20},
+      {isAnswerRight: true, spendedTime: 20},
+      {isAnswerRight: true, spendedTime: 40},
+      {isAnswerRight: true, spendedTime: 20},
+      {isAnswerRight: true, spendedTime: 20},
+      {isAnswerRight: true, spendedTime: 20},
+      {isAnswerRight: true, spendedTime: 20},
+      {isAnswerRight: true, spendedTime: 40}], 3));
   });
 
   it(`Should count -2 points for wrong answer`, () => {
-    assert.strictEqual(16, countResultScore([{isAnswerRight: true, spendedTime: 20000},
-      {isAnswerRight: true, spendedTime: 20000},
-      {isAnswerRight: true, spendedTime: 20000},
-      {isAnswerRight: true, spendedTime: 20000},
-      {isAnswerRight: true, spendedTime: 20000},
-      {isAnswerRight: true, spendedTime: 20000},
-      {isAnswerRight: false, spendedTime: 20000},
-      {isAnswerRight: true, spendedTime: 20000},
-      {isAnswerRight: true, spendedTime: 20000},
-      {isAnswerRight: true, spendedTime: 20000}], 2));
-    assert.strictEqual(12, countResultScore([{isAnswerRight: true, spendedTime: 20000},
-      {isAnswerRight: true, spendedTime: 20000},
-      {isAnswerRight: true, spendedTime: 20000},
-      {isAnswerRight: true, spendedTime: 20000},
-      {isAnswerRight: true, spendedTime: 20000},
-      {isAnswerRight: true, spendedTime: 20000},
-      {isAnswerRight: true, spendedTime: 20000},
-      {isAnswerRight: true, spendedTime: 20000},
-      {isAnswerRight: false, spendedTime: 20000},
-      {isAnswerRight: false, spendedTime: 40000}], 1));
-    assert.strictEqual(11, countResultScore([{isAnswerRight: true, spendedTime: 20000},
-      {isAnswerRight: true, spendedTime: 20000},
-      {isAnswerRight: true, spendedTime: 20000},
-      {isAnswerRight: true, spendedTime: 20000},
-      {isAnswerRight: true, spendedTime: 40000},
-      {isAnswerRight: true, spendedTime: 20000},
-      {isAnswerRight: true, spendedTime: 20000},
-      {isAnswerRight: true, spendedTime: 20000},
-      {isAnswerRight: false, spendedTime: 20000},
-      {isAnswerRight: false, spendedTime: 40000}], 1));
+    assert.strictEqual(16, countResultScore([{isAnswerRight: true, spendedTime: 20},
+      {isAnswerRight: true, spendedTime: 20},
+      {isAnswerRight: true, spendedTime: 20},
+      {isAnswerRight: true, spendedTime: 20},
+      {isAnswerRight: true, spendedTime: 20},
+      {isAnswerRight: true, spendedTime: 20},
+      {isAnswerRight: false, spendedTime: 20},
+      {isAnswerRight: true, spendedTime: 20},
+      {isAnswerRight: true, spendedTime: 20},
+      {isAnswerRight: true, spendedTime: 20}], 2));
+    assert.strictEqual(12, countResultScore([{isAnswerRight: true, spendedTime: 20},
+      {isAnswerRight: true, spendedTime: 20},
+      {isAnswerRight: true, spendedTime: 20},
+      {isAnswerRight: true, spendedTime: 20},
+      {isAnswerRight: true, spendedTime: 20},
+      {isAnswerRight: true, spendedTime: 20},
+      {isAnswerRight: true, spendedTime: 20},
+      {isAnswerRight: true, spendedTime: 20},
+      {isAnswerRight: false, spendedTime: 20},
+      {isAnswerRight: false, spendedTime: 40}], 1));
+    assert.strictEqual(11, countResultScore([{isAnswerRight: true, spendedTime: 20},
+      {isAnswerRight: true, spendedTime: 20},
+      {isAnswerRight: true, spendedTime: 20},
+      {isAnswerRight: true, spendedTime: 20},
+      {isAnswerRight: true, spendedTime: 40},
+      {isAnswerRight: true, spendedTime: 20},
+      {isAnswerRight: true, spendedTime: 20},
+      {isAnswerRight: true, spendedTime: 20},
+      {isAnswerRight: false, spendedTime: 20},
+      {isAnswerRight: false, spendedTime: 40}], 1));
   });
 });
 
@@ -135,7 +135,7 @@ describe(`getResultMessage function`, () => {
     assert.strictEqual(`У вас закончились все попытки. Ничего, повезет в следующи раз!`, getResultMessage([4, 5, 8, 10, 11], {
       score: 12,
       attemptsLeft: 0,
-      timeLeft: 5000
+      timeLeft: 5
     }));
   });
 
@@ -143,12 +143,12 @@ describe(`getResultMessage function`, () => {
     assert.strictEqual(`Вы заняли 1 место из 5. Это лучше, чем у 80% игроков`, getResultMessage([5, 8, 10, 11], {
       score: 12,
       attemptsLeft: 1,
-      timeLeft: 5000
+      timeLeft: 5
     }));
     assert.strictEqual(`Вы заняли 4 место из 10. Это лучше, чем у 60% игроков`, getResultMessage([2, 18, 4, 13, 5, 15, 8, 10, 11], {
       score: 12,
       attemptsLeft: 1,
-      timeLeft: 5000
+      timeLeft: 5
     }));
   });
 });
@@ -156,25 +156,30 @@ describe(`getResultMessage function`, () => {
 describe(`getTimer function`, () => {
   it(`Should return timer object with setted time`, () => {
     assert.isObject(getTimer());
-    assert.strictEqual(5000, getTimer(5000).time);
+    assert.strictEqual(600, getTimer(600).time);
   });
 
   it(`Tick method should decrease time by tick time`, () => {
-    assert.strictEqual(5000, getTimer(6000).tick());
-    assert.strictEqual(6000, getTimer(8000, 2000).tick());
+    assert.strictEqual(59, getTimer(60).tick());
   });
 
   it(`Should return -1 when time is over`, () => {
-    assert.strictEqual(-1, getTimer(1000).tick());
-    assert.strictEqual(-1, getTimer(2000, 2000).tick());
+    assert.strictEqual(-1, getTimer(1).tick());
   });
 
   it(`Shouldn't set negative time`, () => {
     assert.throws(() => {
       getTimer().time = -1000;
     });
-    assert.throws(() => {
-      getTimer().tickTime = -1000;
-    });
+  });
+});
+
+describe(`getTimeString function`, () => {
+  it(`Should return correct time string`, () => {
+    assert.deepEqual(`за 1 минуту и 01 секунду`, getTimeString(61));
+    assert.deepEqual(`за 2 минуты и 22 секунды`, getTimeString(142));
+    assert.deepEqual(`за 3 минуты и 37 секунд`, getTimeString(217));
+    assert.deepEqual(`за 4 минуты и 11 секунд`, getTimeString(251));
+    assert.deepEqual(`за 5 минут и 00 секунд`, getTimeString(300));
   });
 });
